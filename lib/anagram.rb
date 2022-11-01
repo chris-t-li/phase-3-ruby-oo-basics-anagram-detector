c926e9be2b13425fc0ab3773017cf7c4dcb2d6cc
Chris Li <chris.lty07@gmail.com>
2022-10-31 22:43:35 -0400

    commit tree f2cb40bd0549c059127252fa482b723716a96690

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+require "pry"
+class Anagram 
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        return_array = array.select { |str| str.split("").sort == word.split("").sort}
+        
+        #binding.pry
+        
+    end
+
+end
+
+listen = Anagram.new("listen")
+
+listen.match(%w[enlists google inlets banana netsil lniest])
